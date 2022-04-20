2c4f9d904a6f8a0919df92dea551394df22b5bc6
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 21:13:40 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -4,7 +4,7 @@ st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 weight=st.slider('How fat are you in kgs?', 0, 130, 10)
-height=st.slider('How tall are you in kgs?', 0, 200, 10)
+height=st.slider('How tall are you in cms?', 0, 200, 10)
 columnnames=["height","weight"]
 size=pd.DataFrame(columns=columnnames)
 
