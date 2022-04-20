466ff85e90304aae91760fb48bdc62e5dc09d3d5
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 22:33:24 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -10,7 +10,7 @@ height=st.slider('How tall are you in cms?', 0, 200, 10)
 
 
 if st.button("Save it"):
-    size.df=size.append({"height": height,"weight":weight})
+    size.df=size.append({"height": height,"weight":weight},ignore_index=True)
     st.text("Updated dataframe")
     size=size.df
     st.dataframe(size)
