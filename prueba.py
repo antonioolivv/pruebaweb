5265510aa45846af7173bdeffe16d1b4d7e141bc
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 22:30:55 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,10 +3,11 @@ import pandas as pd
 st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
+columnnames=["height","weight"]
 size=pd.DataFrame(columns=columnnames)
 weight=st.slider('How fat are you in kgs?', 0, 130, 10)
 height=st.slider('How tall are you in cms?', 0, 200, 10)
-columnnames=["height","weight"]
+
 
 if st.button("Save it"):
     size.df=size.append({"height": height,"weight":weight},ignore_index=True)
