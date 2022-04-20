e9f59f60b302922eb825b3d33d24296dbb510e14
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 20:42:22 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,3 +3,6 @@ import pandas as pd
 st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
+weight=st.slider('How fat are you in kgs?', 0, 130, 10)
+height=st.slider('How tall are you in kgs?', 0, 200, 10)
+st.metric("This is you",weight,height)
