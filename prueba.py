9623e37db3561a9dcfd987702f675391ae726b4f
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 22:50:57 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -5,14 +5,15 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 columnnames=["height","weight"]
 size=pd.DataFrame(columns=columnnames)
-weight=st.slider('How fat are you in kgs?', 0, 130, 10)
-height=st.slider('How tall are you in cms?', 0, 200, 10)
 for i in range(5):
-    if st.button("Save it"):
-        size.df=size.append({"height": height,"weight":weight},ignore_index=True)
-        st.text("Updated dataframe")
-        size=size.df
-        st.dataframe(size)
+
+    weight=st.slider('How fat are you in kgs?', 0, 130, 10)
+    height=st.slider('How tall are you in cms?', 0, 200, 10)
+        if st.button("Save it"):
+            size.df=size.append({"height": height,"weight":weight},ignore_index=True)
+            st.text("Updated dataframe")
+            size=size.df
+            st.dataframe(size)
  
                                            
                                             
