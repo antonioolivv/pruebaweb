0353305079687f9e4402dd8733df28f27f07a2d1
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 21:01:39 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -5,5 +5,14 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 weight=st.slider('How fat are you in kgs?', 0, 130, 10)
 height=st.slider('How tall are you in kgs?', 0, 200, 10)
-size=dataframe("height":[height],"weight":[weight])
+size=dataframe("height":[],"weight":[])
+session_state = SessionState.get(df=size)
+
+if st.button("Save it"):
+  session_state.df=session_state.df.append({"height": height,"weight":weight}ignore_index=True)
+  st.text("Updated dataframe")
+  st.dataframe(session_state.df)
+                                           
+                                            
+  
 st.bar_chart(size)
