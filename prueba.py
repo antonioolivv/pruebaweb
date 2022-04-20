b1e22da8432c93b44affd41817996ec4d548b444
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-20 22:24:22 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd 
 st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
-st.write(df)
-weight=st.slider('How fat are you in kgs?', 0, 130, 10)
+st.write(df)fweight=st.slider('How fat are you in kgs?', 0, 130, 10)
 height=st.slider('How tall are you in cms?', 0, 200, 10)
 columnnames=["height","weight"]
 size=pd.DataFrame(columns=columnnames)
 
-if st.button("Save it"):
-  size=size.append({"height": height,"weight":weight},ignore_index=True)
-  st.text("Updated dataframe")
-  st.dataframe(size.df)
+while len(df.index)>10:
+  if st.button("Save it"):
+    size.df=size.append({"height": height,"weight":weight},ignore_index=True)
+    st.text("Updated dataframe")
+    st.dataframe(size.df)
 
                                            
                                             
