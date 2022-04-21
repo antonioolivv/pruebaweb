e7cb9868784427bf8ded7d9b9c8042474c18e374
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-04-21 17:21:01 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,7 +6,6 @@ st.write(df)
 columnnames=["height","weight"]
 size=pd.DataFrame(columns=columnnames)
 
-
 weight=st.slider('How fat are you in kgs?', 0, 130, 10)
 height=st.slider('How tall are you in cms?', 0, 200, 10)
 if st.button("Save it"):
@@ -14,7 +13,8 @@ if st.button("Save it"):
     st.text("Updated dataframe")
     size=size.df
     st.dataframe(size)
- 
-bmi=(height/((weight)**2)*100
-print(bmi)                                            
+    
+   
+bmi=((height*100)/((weight)**2)
+st.write(bmi)                                            
 st.bar_chart(size)
